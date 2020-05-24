be372cdc93dcf61d173bb7f44f112d5ecee43ab5
Andrew White <=>
2020-05-24 16:49:12 +0100

    got a basic multi working

---
+++ src/components/QueryBuilder.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <vue-query-builder
+      :rules="rules"
+      v-model="query"
+    />
+    {{ query }}
+  </div>
+</template>
+
+<script>
+import VueQueryBuilder from 'vue-query-builder'
+import QueryBuilderCombo from './QueryBuilderCombo'
+
+export default {
+  name: 'QueryBuilder',
+  data () {
+    return {
+      rules: [
+        {
+          type: 'custom-component',
+          label: 'Multi',
+          component: QueryBuilderCombo,
+          operators: ['=']
+        }
+      ],
+      query: {}
+    }
+  },
+  components: {
+    VueQueryBuilder
+  }
+}
+</script>
---
+++ src/components/QueryBuilderCombo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <input
+        v-model="val1"
+        v-on:input="update_model"
+        >
+      <input
+        v-model="val2"
+        v-on:input="update_model"
+        >
+  </div>
+</template>
+<script>
+export default {
+  name: 'QueryBuilderCombo',
+  methods: {
+    update_model: function () {
+      const value = `${this.$data.val1} ${this.$data.val2}`
+      this.$emit('input', value)
+    }
+  },
+  data () {
+    return {
+      val1: '',
+      val2: '',
+      operators: []
+    }
+  }
+}
+</script>
